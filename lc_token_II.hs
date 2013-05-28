41d794f0adc875e345e1ddac497fbaeaec204210
czel <czel@mac.com>
2013-05-29 05:33:39 +0900

    Change output of tokenizer to Token from String.

--- lc_token_II.hs
+++ lc_token_II.hs
@@ -1,4 +1,8 @@
 {-# LANGUAGE FlexibleContexts #-}
+module Lc.Parse(
+	Token, lc_file
+) where
+
 import Text.Parsec
 import Text.Parsec.String (GenParser)
 import Text.Parsec.String (Parser)
@@ -8,6 +12,8 @@ import Data.Char
 import Control.Monad
 import Numeric 
 
+----
+--Token
 
 --Keyword : Predefined keywords like "struct" or "func".
 --User_token : Identifiers possibly defined by users.
@@ -16,13 +22,14 @@ import Numeric
 --                       like 3'b01_1.
 --Meta_number_literal v u: Meta number constants With value v and unit u.
 
-
 data Token = 
 	User_token String |
+	Predefined_token String |
 	Keyword String |
 	Symbol String |
 	Bit_array_literal Int Int |
 	Meta_number_literal Int String
+	deriving (Show)
 	
 white_space :: Parser Char 
 white_space = space <|> newline <|> tab
@@ -52,11 +59,27 @@ block_comment = do
 --skip comment and white space
 skip_cws = skipMany $ ((skipMany1 white_space) <|> (skipMany1 comment))
 
-lc_user_token :: Parser String
+lc_user_token :: Parser Token 
 lc_user_token = do
 	x <- letter
 	xs <- many (letter <|> char '_' <|> digit)
-	return (x:xs) 
+	return $ User_token (x:xs)
+
+---------
+--symbols&keywords
+lc_symbol = do
+	x <- choice (map (try.string) double_char_symbol) <|>
+	     choice (map (try.string) single_char_symbol)
+	return $ Symbol x
+
+lc_keyword = do
+	x <- choice (map (try.string) keywords)
+	return $ Keyword x
+
+lc_predefined_token= do
+	x <- choice (map (try.string) pre_defined_tyes) <|>
+	     choice (map (try.string) pre_defined_meta_tyes)
+	return $ Predefined_token x
 
 -------------------
 ---Number literals
@@ -77,43 +100,47 @@ lc_number_body b val = do
 		white_space  
 		return new
 
-lc_number::Parser String
+lc_number::Parser Token 
 lc_number = do
 	bit_width <- many digit
 	char '\''
 	base_code <- oneOf "bBoOdDhH"
 	value <- f base_code
-	return $ bit_width ++ "-" ++ show value
+	return $ Bit_array_literal (read bit_width) value
 	where f b
 		| elem b "bB" = lc_number_body 2 0
 		| elem b "oO" = lc_number_body 8 0
 		| elem b "dD" = lc_number_body 10 0
 		| elem b "hH" = lc_number_body 16 0
 
-lc_metanum::Parser String
-lc_metanum = do 
+lc_metanum_body::Parser Int 
+lc_metanum_body = do 
 	msb <- oneOf "123456789"
 	lest <- many digit
-	return (msb:lest)
+	return $ read (msb:lest)
+
+lc_metanum = do 
+	val <- lc_metanum_body
+	return $ Meta_number_literal val ""
 
 si_prefix::Parser Char 
 si_prefix = oneOf "TGMkmunp"
 
 lc_metafreq = do
-	val <- lc_metanum
+	val <- lc_metanum_body
 	unit <- choice [
 		try $ do {p<-si_prefix; u<-string "Hz"; return (p:u)},
 		try $ string "Hz"]
-	return $ val ++ unit
+	return $ Meta_number_literal val unit
 
 lc_metatime = do
-	val <- lc_metanum
+	val <- lc_metanum_body
 	unit <- choice [
 		try $ do {p<-si_prefix; u<-string "s"; return (p:u)},
 		try $ string "s"]
-	return $ val ++ unit
+	return $ Meta_number_literal val unit
 
-lc_literal::Parser String
+lc_literal::Parser Token 
 lc_literal = choice [
 		try lc_number,
 		try lc_metafreq,
@@ -121,23 +148,22 @@ lc_literal = choice [
 		lc_metanum
 		]
 
+lc_token::Parser Token 
 lc_token = do
 	t <- choice [
-		choice (map (try.string) double_char_symbol),
-		choice (map (try.string) single_char_symbol),
-		choice (map (try.string) keywords),
-		choice (map (try.string) pre_defined_tyes),
-		choice (map (try.string) pre_defined_meta_tyes),
+		lc_symbol,
+		lc_keyword,
+		lc_predefined_token,
 		try lc_literal,
 		lc_user_token
 		]
 	skip_cws
 	return t
 
-lc_tokens::[String] -> Int -> Parser [String]
-lc_tokens s n  
-	| n == 0 = do {last<-lc_token; return (s++[last])}
-	| otherwise = do {x<-lc_token; lc_tokens (s++[x]) (n-1);}
+--lc_tokens::[String] -> Int -> Parser [String]
+--lc_tokens s n  
+	-- | n == 0 = do {last<-lc_token; return (s++[last])}
+	-- | otherwise = do {x<-lc_token; lc_tokens (s++[x]) (n-1);}
 
 lc_file = do
 	skip_cws
